f2224be29d11263dd5554e38c66ad519e5614215
rafaelagoliveira <rafaelaoliveira.dev@gmail.com>
2023-06-25 20:09:42 -0400

    :heavy_minus_sign: Build
    Build folder removed

--- 2023/PP/simpleListDeque/build/CMakeFiles/simpleNodeDeque.dir/Deque/src/Deque.cpp.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/simpleNodeDeque.dir/Deque/src/Deque.cpp.o: \
- /home/tanjerina/Documentos/CC/CC-2023/PP/simpleListDeque/Deque/src/Deque.cpp \
- /usr/include/stdc-predef.h \
- /home/tanjerina/Documentos/CC/CC-2023/PP/simpleListDeque/Deque/include/Deque.h \
- /home/tanjerina/Documentos/CC/CC-2023/PP/simpleListDeque/Deque/include/Node.h \
- /home/tanjerina/Documentos/CC/CC-2023/PP/simpleListDeque/Deque/src/Node.cpp \
- /home/tanjerina/Documentos/CC/CC-2023/PP/simpleListDeque/Deque/include/Node.h \
- /home/tanjerina/Documentos/CC/CC-2023/PP/simpleListDeque/Deque/src/Deque.cpp
--- 2023/PP/simpleListDeque/build/CMakeFiles/simpleNodeDeque.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for simpleNodeDeque.
-# This may be replaced when dependencies are built.
